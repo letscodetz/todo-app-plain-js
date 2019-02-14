093bbe8135fb4ed6014871660beeb8196bbf668c
mloweedgar <mloweedgar0@gmail.com>
2019-02-14 15:53:56 +0300

    feat(main.js): add remove todo logic
    add logic to remove todo and logic to update todo count

--- js/main.js
+++ js/main.js
@@ -1,73 +1,71 @@
-const allToDo = [];
+let allToDo = [];
 
-
-const getCreatedAtDate = function() {
-    const today = new Date();
-    return today.toLocaleString();
-}
-
-const setCountValue = function() {
-    const countElement = document.getElementById("total");
-    const totalTodos = allToDo.length;
-    countElement.innerHTML = totalTodos;
-}
-const createToDo = function (text) {
-    // button for removing todo
-    const removeTodoButton = document.createElement("button");
-    removeTodoButton.setAttribute("id", "remove-todo");
-    removeTodoButton.innerHTML = "x";
-    const id = allToDo.indexOf(text);
-
-    // div for showing date todo was created
-    const dateElement = document.createElement("div");
-    const createdAt = getCreatedAtDate();
-    dateElement.innerHTML =createdAt;
-
-    // todo description text
-    const description = document.createElement("div");
-    description.setAttribute("class", "description");
-    description.innerHTML = text;
-
-    const itemActions = document.createElement("div");
-    itemActions.setAttribute("class", "item-actions");
-    itemActions.appendChild(dateElement);
-    itemActions.appendChild(removeTodoButton);
-
-     // container  for  a todo item
-     const itemContainer = document.createElement("div");
-     itemContainer.setAttribute("class", "list-item");
-     itemContainer.setAttribute("id", id);
-     itemContainer.appendChild(description);
-     itemContainer.appendChild(itemActions);
-
-     // getting the list container
-     const listContainer = document.getElementsByClassName("list-contents")[0];
-     listContainer.appendChild(itemContainer);
-
-}
-
-const addToDo = function () {
+const onAddTodo = function () {
     const input = document.getElementById("input-todo");
     const value = input.value;
     allToDo.push(value);
     createToDo(value);
     setCountValue();
+
+    // clears the input
+    input.value = '';
 }
 
-const displayToDos = function (allToDo) {
-    allToDo.map(function(todo) {
-        createToDo(todo);
-    });
+const OnRemoveTodo = function (e) {
+    //  id of todo
+    const id = e.target.getAttribute('id');
 
+    // get the list container
+    const listContainer = document.getElementsByClassName("list-contents")[0];
+    const todoToRemove = document.getElementById(`list-item-${id}`);
+    const text = todoToRemove.getElementsByClassName('description')[0].innerHTML;
+
+    // remove todo from the html file
+    listContainer.removeChild(todoToRemove);
+
+    //  remove todo entry from array of all todo
+    allToDo = allToDo.filter(function(todo) {
+        return todo !== text;
+    });
     setCountValue();
 }
 
-const attachEventListenersToElements = function () {
-    const addButton = document.getElementById("add-todo");
-    addButton.addEventListener("click", addToDo);
+const createToDo = function (text) {
+    const currentDate = getCreatedAtDate();
+    const id = allToDo.indexOf(text);
+    const todo = `
+        <div class="description">${text}</div>
+        <div class="item-actions">
+            <div>${currentDate}</div>
+            <button id="${id}">x</button>
+        </div>
+    `
+    // container  for  a todo item
+    const itemContainer = document.createElement("div");
+    itemContainer.setAttribute("class", "list-item");
+    itemContainer.setAttribute("id", `list-item-${id}`);
+    itemContainer.innerHTML = todo;
+    // getting the list container
+    const listContainer = document.getElementsByClassName("list-contents")[0];
+    listContainer.appendChild(itemContainer);
+    document.getElementById(`${id}`).addEventListener('click', OnRemoveTodo);
 }
 
-displayToDos(allToDo);
+const setCountValue = function () {
+    const totalTodos = allToDo.length;
+    document.getElementById("total").innerHTML = totalTodos;
+}
 
-attachEventListenersToElements();
+const getCreatedAtDate = function () {
+    const today = new Date();
+    return today.toLocaleString();
+}
+
+const addTodoEventListener = function () {
+    document.getElementById("add-todo").addEventListener("click", onAddTodo);
+}
+
+
+addTodoEventListener();
+setCountValue();
 
