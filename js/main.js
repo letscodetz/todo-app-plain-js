ac2ba82f7bf99b7a5a7ca96972a214137dc09b89
mloweedgar <mloweedgar0@gmail.com>
2019-02-25 20:55:20 +0300

    refactor(main.js): remove unncessary function

--- js/main.js
+++ js/main.js
@@ -1,86 +1,33 @@
-// stores all todo in array
-let allToDo = [];
-
-// removes todo event listener
-// receives the id of remove todo button
-addClickEventListenerToRemoveTodoButton = function (index) {
-    const removeTodoButton = document.getElementById(index);
-    removeTodoButton.addEventListener('click', removeTodo);
-}
-
-// adds event listener for adding new todo
-const addTodoEventListener = function () {
-    const plusButton = document.getElementById("add-todo");
-    plusButton.addEventListener("click", createToDo);
-}
-
-const removeTodoTextFromArray = function (text) {
-    allToDo = allToDo.filter(function (todo) {
-        return todo !== text;
-    });
-}
-
-const getTodoContents = function () {
+// creates a todo item
+const createToDo = function () {
 
     // get value typed into todo
     const input = document.getElementById("input-todo");
     const text = input.value;
+
     // clears the input
     input.value = '';
-    allToDo.push(text);
-    const id = allToDo.indexOf(text);
-    const todoContents = { description: text, todoId: id };
 
-    return todoContents;
-}
-
-const createTodoListItem = function (text, index) {
-    // create todo item description
-    const itemDescription = document.createElement("div");
-    itemDescription.innerHTML = text;
-
-    // create remove todo button
-    const removeTodoButton = document.createElement("button");
-
-    // assign todo id to the remove todo button
-    removeTodoButton.setAttribute("id", index);
-    removeTodoButton.innerHTML = 'x';
+    // create todo list item contents
+    const listItemContents = `<div>${text}</div>
+    <button onclick="removeTodo(event)">x</button>`;
 
     // create todo list item
     const listItem = document.createElement("li");
 
-    // add class  attribute to the 
-    // todo list item
+    // add class attribute to list item element
     listItem.setAttribute("class", "list-item");
 
-    // append todo description and remove todo button
-    // to todo list item
-    listItem.appendChild(itemDescription);
-    listItem.appendChild(removeTodoButton);
+    // add todo contents to the list newly created 
+    // list item element
+    listItem.innerHTML = listItemContents;
 
-    return listItem;
-}
-
-
-// creates a todo item
-const createToDo = function () {
-
-    // get the text typed by user and index of todo
-    const todoContents = getTodoContents(); // returns that text and id of the you typed
-    const text = todoContents.description;
-    const index = todoContents.todoId;
-
-    // create todo list item
-    const listItem = createTodoListItem(text, index);
 
     // get the todo list 
     const todoList = document.querySelector("ul");
 
     // append list item to the todo list
     todoList.appendChild(listItem);
-
-    // attach event listener for removing todo
-    addClickEventListenerToRemoveTodoButton(index);
 }
 
 
@@ -99,16 +46,5 @@ const removeTodo = function (event) {
 
     // remove todo from the list
     todoList.removeChild(listItemToRemove);
-
-
-    const divForText = listItemToRemove.querySelector('div');
-    const text = divForText.innerHTML;
-
-
-    //  remove todo from array of all todo
-    removeTodoTextFromArray(text);
 }
-
-// initializing todo
-addTodoEventListener();
 
